bd1bba19b73c98a26454aaf324a9d40d91ff3eb9
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-05-15 19:40:05 -0400

    linting fixes

--- src/containers/Playlist.js
+++ src/containers/Playlist.js
@@ -1,7 +1,10 @@
+/* eslint-disable react/destructuring-assignment */
 // playlist component
 
 import React, { Component } from 'react';
-import {View, Text, Button, StyleSheet, ScrollView} from 'react-native';
+import {
+  View, Text, Button,
+} from 'react-native';
 
 class Playlist extends Component {
   constructor(props) {
@@ -10,42 +13,45 @@ class Playlist extends Component {
       songs: [],
     };
   }
+
   onBackClick() {
-      console.log('onBackClick')
+    console.log('onBackClick');
+    console.log(this.state.songs);
   }
+
   onAddClick() {
-      console.log('onAddClick')
+    console.log('onAddClick');
   }
 
   render() {
     return (
-        <View>
-            <View id="top">
-                <Button title="back" onPress={this.onAddClick}/>
-                <Text>
+      <View>
+        <View id="top">
+          <Button title="back" onPress={this.onAddClick} />
+          <Text>
                     Playlist Title
-                    {"\n"}
+            {'\n'}
                     Location
-                </Text>
-            </View>
-            <View id="songList">
-                <Text>
-                    Song 1
-                    {"\n"}
-                    {"\n"}
-                    Song 2
-                    {"\n"}
-                    {"\n"}
-                    Song 3
-                </Text>
-            </View>
-            <View id="songBar">
-                <Text>
-                    Song Bar Here
-                </Text>
-            </View>
-            <Button title="add" onPress={this.onAddClick}/>
+          </Text>
         </View>
+        <View id="songList">
+          <Text>
+                    Song 1
+            {'\n'}
+            {'\n'}
+                    Song 2
+            {'\n'}
+            {'\n'}
+                    Song 3
+          </Text>
+        </View>
+        <View id="songBar">
+          <Text>
+                    Song Bar Here
+          </Text>
+        </View>
+        <Button title="add" onPress={this.onAddClick} />
+      </View>
     );
   }
 }
--- src/containers/Song.js
+++ src/containers/Song.js
@@ -1,51 +1,57 @@
+/* eslint-disable react/destructuring-assignment */
 // add song component
 
 import React, { Component } from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  View, Text, Button, TextInput,
+} from 'react-native';
 
 class AddSong extends Component {
   constructor(props) {
     super();
     this.state = {
-        search: '',
+      search: '',
     };
+
+    this.onSearchChange = this.onSearchChange.bind(this);
   }
 
   onBackClick() {
-      console.log('onBackClick');
+    console.log('onBackClick');
   }
 
   onAddClick() {
-      console.log('onAddClick');
+    console.log('onAddClick');
   }
 
-    onSearchChange(event) {
-        console.log('onSearchChange');
-        this.setState({ search: event.target.value });
-    }
+  onSearchChange(event) {
+    console.log('onSearchChange');
+    this.setState({ search: event.target.value });
+    console.log(this.state.search);
+  }
 
   render() {
     return (
-        <View>
-            <View id="top">
-                <Button title="back" onPress={this.onBackClick}/>
-                <Text>
+      <View>
+        <View id="top">
+          <Button title="back" onPress={this.onBackClick} />
+          <Text>
                     Add a Song
-                </Text>
-            </View>
-            <View id="searchbar">
-                <TextInput
-                    placeholder="Search for a song"
-                    onChangeText={this.onSearchChange}
-                />
-            </View>
-            <View id="results">
-                <Text>
-                    Results list here
-                </Text>
-            </View>
-            <Button title="add" onPress={this.onAddClick}/>
+          </Text>
         </View>
+        <View id="searchbar">
+          <TextInput
+            placeholder="Search for a song"
+            onChangeText={this.onSearchChange}
+          />
+        </View>
+        <View id="results">
+          <Text>
+                    Results list here
+          </Text>
+        </View>
+        <Button title="add" onPress={this.onAddClick} />
+      </View>
     );
   }
 }
