aa505cda5acec9864362a53786110745c20a42da
Emma Langfitt <emma.g.langfitt.20@dartmouth.edu>
2019-05-22 23:17:39 -0400

    working locally

--- src/reducers/playlist-reducer.js
+++ src/reducers/playlist-reducer.js
@@ -1,11 +1,13 @@
 import { ActionTypes } from '../actions';
 
-const PlaylistReducer = (state = { all: [], current: '' }, action) => {
+const PlaylistReducer = (state = { all: [], current: '', playstate: '' }, action) => {
   switch (action.type) {
     case ActionTypes.FETCH_PLAYLISTS:
       return Object.assign({}, state, { all: action.payload.all });
     case ActionTypes.FETCH_PLAYLIST:
       return Object.assign({}, state, { current: action.payload.current });
+    case ActionTypes.PLAYSTATE:
+      return Object.assign({}, state, { playstate: action.payload });
     default:
       return state;
   }
