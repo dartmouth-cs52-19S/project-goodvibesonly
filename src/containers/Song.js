4ef3812c7a18becf8c3398beb98ef1ea8cf0ba47
Angela Li <alidartmouth20@gmail.com>
2019-05-25 23:17:14 -0400

    song search without render

--- src/containers/Song.js
+++ src/containers/Song.js
@@ -4,32 +4,55 @@
 
 import React, { Component } from 'react';
 import {
-  View, Text, TouchableOpacity, TextInput, StyleSheet, ImageBackground,
+  View, Text, TouchableOpacity, TextInput, StyleSheet, ImageBackground, Image,
 } from 'react-native';
+import axios from 'axios';
+import { connect } from 'react-redux';
+import { fetchPlaylists } from '../actions';
 
 class AddSong extends Component {
   constructor(props) {
     super();
     this.state = {
       search: '',
+      results: null,
     };
-
-    this.onSearchChange = this.onSearchChange.bind(this);
-    this.onAddClick = this.onAddClick.bind(this);
   }
 
-  onAddClick() {
+  onAddClick = () => {
     console.log('onAddClick');
     this.props.navigation.pop();
   }
 
-  onSearchChange(event) {
+  onSearchChange = (text) => {
     console.log('onSearchChange');
-    this.setState({ search: event.target.value });
-    console.log(this.state.search);
+    this.setState({ search: text });
+  }
+
+  onSearchClick = () => {
+    const API_TRACK_URL = 'https://api.spotify.com/v1/search';
+
+    const params = {
+      q: this.state.search,
+      type: 'track',
+      market: 'US',
+      limit: 5,
+    };
+
+    console.log('bout to do search api call');
+    axios.get(`${API_TRACK_URL}`, { headers: { authorization: `Bearer ${this.props.token}` }, params })
+      .then((response) => {
+        this.setState({ results: response.data.tracks.items });
+      })
+      .catch((error) => {
+        console.log('`spotify api error`');
+      });
   }
 
   render() {
+    if (this.state.results !== null) {
+      console.log(this.state.results);
+    }
     return (
       <View>
         <ImageBackground source={require('../img/Create.png')} style={styles.backgroundImage}>
@@ -41,9 +64,14 @@ class AddSong extends Component {
           <View id="searchbar" style={styles.info}>
             <TextInput
               placeholder="Search for a song"
-              onChange={this.onSearchChange}
+              onChangeText={this.onSearchChange}
               style={styles.input}
             />
+            <TouchableOpacity onPress={this.onSearchClick} style={styles.searchButton}>
+              <Image
+                source={require('../img/search.png')}
+              />
+            </TouchableOpacity>
           </View>
           <View id="results">
             {/* <Text>
@@ -87,6 +115,15 @@ const styles = StyleSheet.create({
     height: 40,
     padding: 10,
   },
+  searchButton: {
+    backgroundColor: '#1DB5E5',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    width: 40,
+    height: 40,
+    padding: 10,
+  },
   input: {
     height: 60,
     width: 200,
@@ -102,7 +139,19 @@ const styles = StyleSheet.create({
   },
   info: {
     padding: 30,
+    flexDirection: 'row',
+    alignItems: 'center',
   },
 });
 
-export default AddSong;
+function mapStateToProps(reduxState) {
+  return {
+    token: reduxState.auth.token,
+  };
+}
+
+const mapDispatchToProps = {
+  fetchPlaylists,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddSong);
